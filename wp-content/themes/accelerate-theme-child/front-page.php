57eac7d294f9d17c3e574ca091a1461c621086aa
kimberlythomas <thomas.kimberlym@gmail.com>
2022-08-20 09:49:20 -0700

    Added Twitter module to homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -63,4 +63,14 @@ get_header(); ?>
 	</div>
 </section>
 
+<!-- Sidebar -->
+
+<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+<div id="secondary" class="widget-area" role="complementary">
+
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+<p><a href="https://twitter.com/skillcrush" class="follow-us-link">Follow Us ›</a></p>
+</div>
+<?php endif; ?>
+
 <?php get_footer(); ?>
